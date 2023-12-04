82b90ffd400569470e230795fda5cb380b5bc3e8
wsydn <wsydney@web.de>
2023-12-04 09:43:47 +0100

    More languages

--- config/custom.php
+++ config/custom.php
@@ -13,5 +13,19 @@ return [
         'Berlin',
         'Hamburg',
         'Soltau'
+    ],
+
+    // Language names for site switcher
+    // site.locale.displayName is not suitable for the frontend (e.g. "German (Germany)")
+    // Trying to add some logic to the template to handle this seems to be messy.
+    // This must match the languages in Craft, otherwise errors will occur
+    'languageNames' => [
+        'de-DE' => 'Deutsch',
+        'en-US' => 'English',
+        'he-IL' => 'עברית',
+        'it-IT' => 'Italiano',
+        'ja-JP' => '日本語',
+        'tr-TR' => 'Türkçe',
+        'zh-Hans-CN' => '中文',
     ]
 ];
