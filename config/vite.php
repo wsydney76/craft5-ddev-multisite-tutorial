85133a54aff1262b3e00282c3241ffc054b85131
wsydn <wsydney@web.de>
2024-07-16 11:05:07 +0200

    fix

--- config/vite.php
+++ config/vite.php
@@ -7,9 +7,9 @@ $useDevServer = App::env('CRAFT_ENVIRONMENT') === 'dev' && App::env('VITE_USE_DE
 return [
     'checkDevServer' => $useDevServer,
     'devServerInternal' => 'http://localhost:3000/',
-    'devServerPublic' => Craft::getAlias('@web') . ':3000',
+    'devServerPublic' => Craft::getAlias('@weburl') . ':3000',
     'errorEntry' => 'resources/js/app.js',
     'manifestPath' => Craft::getAlias('@webroot') . '/dist/assets/manifest.json',
-    'serverPublic' => Craft::getAlias('@web')  . '/dist/assets/',
+    'serverPublic' => Craft::getAlias('@weburl')  . '/dist/assets/',
     'useDevServer' => $useDevServer,
 ];
